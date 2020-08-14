ad916c1e99dddeedecf0c72704b148f6d22e19d4
Tom Rizzi <tcr59@uclive.ac.nz>
2020-08-15 10:15:28 +1200

    Updated pisspricer api module

--- api.py
+++ api.py
@@ -6,15 +6,14 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-global url
-global token
 url = os.getenv('pisspricer.url')
 email = os.environ.get('pisspricer.email')
 password = os.environ.get('pisspricer.password')
 
 res = requests.post(url + '/users/login', json={"email": email, "password": password})
+
 if not res.ok:
-    raise Exception("Connection to API failed")
+    raise Exception("Login to API failed")
 res_json = res.json()
 token = res_json["authToken"]
 headers = {"X-Authorization": token}
