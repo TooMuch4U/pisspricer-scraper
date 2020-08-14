300fbcba310feb7948fb620e88fc14ac04936347
Tom Rizzi <tcr59@uclive.ac.nz>
2020-08-15 10:15:04 +1200

    Updated generic_store print function

--- stores/generic_store.py
+++ stores/generic_store.py
@@ -1,13 +1,9 @@
 import tools
 import os
-import sys
 from abc import ABC, abstractmethod
 
 
 class Store(ABC):
-
-    def __init__(self):
-        pass
 
     @abstractmethod
     def update_all_items(self):
@@ -15,21 +11,25 @@ class Store(ABC):
 
     @staticmethod
     def print_progress(iteration, total, title=""):
-        os.system('clear')
-
-        # Set length for either side of title
+        """
+        Print progress bar
+        :param iteration: Iteration number
+        :param total: Total number of iterations there will be
+        :param title: Title to print on iteration zero
+        :return: None
+        """
         length = 50
-        dash_n = len('Progress:' + 'Complete') + length - len(title)
-        if dash_n % 2 == 0:
-            dash_l = dash_r = dash_n // 2
-        else:
-            dash_l = dash_r = dash_n // 2
-            dash_l += 1
 
         # Print Title
-        print(dash_l * "-" + title + dash_r * "-")
-        sys.stdout.flush()
-
+        if iteration == 0:
+            dash_n = len('Progress:' + 'Complete') + length - len(title)
+            if dash_n % 2 == 0:
+                dash_l = dash_r = dash_n // 2
+            else:
+                dash_l = dash_r = dash_n // 2
+                dash_l += 1
+            os.system('clear')
+            print(dash_l * "-" + title + dash_r * "-")
 
         # Print bar
         tools.print_progress_bar(iteration, total, prefix='Progress:', suffix='Complete', length=length)
